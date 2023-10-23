33026d9755e0d3300693f2ea378c17da1d3e2d0c
reyleal27 <reyvincentleal@gmail.com>
2023-10-23 09:08:16 +0800

    about-product-modal

--- product-modal.js
+++ product-modal.js
@@ -1,12 +1,16 @@
 (() => {
     const refs = {
       openModalBtn: document.querySelector("[product-data-modal-open]"),
+      openIceCoffeeModal: document.querySelector("[product-ice-coffee-data-modal-open]"),
+      openMilkShakeModal: document.querySelector("[product-milk-shake-data-modal-open]"),
       closeModalBtn: document.querySelector("[product-data-modal-close]"),
       modal: document.querySelector("[product-data-modal]"),
     };
   
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
+    refs.openIceCoffeeModal.addEventListener("click", toggleModal);
+    refs.openMilkShakeModal.addEventListener("click",toggleModal);
   
     function toggleModal() {
       refs.modal.classList.toggle("is-hidden");
